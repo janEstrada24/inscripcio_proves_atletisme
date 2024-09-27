f7fe7e60f8ee8f8cec671488a76875074d77ac66
janEstrada24 <jestrada48@boscdelacoma.cat>
2024-09-27 15:52:23 +0200

    "spec.ts" files moved into the new "Test" directory.

--- src/app/Test/registration.component.spec.ts
+++ src/app/Test/registration.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegistrationComponent } from './registration.component';
+import { RegistrationComponent } from '../Components/User/registration/registration.component';
 
 let component: RegistrationComponent;
 let fixture: ComponentFixture<RegistrationComponent>;
